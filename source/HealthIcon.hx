5c31703e0702352698310a8182f18758103a4204
blaidev <85421187+blaidev@users.noreply.github.com>
2021-11-11 17:03:29 -0500

    removed bugs

--- source/HealthIcon.hx
+++ source/HealthIcon.hx
@@ -39,7 +39,6 @@ class HealthIcon extends FlxSprite
 		animation.add('parents-christmas', [17, 18], 0, false, isPlayer);
 		animation.add('monster', [19, 20], 0, false, isPlayer);
 		animation.add('monster-christmas', [19, 20], 0, false, isPlayer);
-		animation.add('tankman', [24, 25], 0, false, isPlayer);
 		animation.play(char);
 		switch(char){
 			case 'bf-pixel' | 'senpai' | 'senpai-angry' | 'spirit' | 'gf-pixel':
